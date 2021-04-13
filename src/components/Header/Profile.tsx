776c51c768ecf1a3c30458ba051f240df6661e6e
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-04-13 13:45:50 -0300

    Responsive Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Gabriel Carvalho</Text>
-        <Text color="gray.300">braga.gabrielcarvalho@gmail.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Gabriel Carvalho</Text>
+          <Text color="gray.300">braga.gabrielcarvalho@gmail.com</Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
