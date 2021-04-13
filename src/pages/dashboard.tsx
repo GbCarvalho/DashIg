c9a4d7fb8daea424241e4d11b4fa4ec99313034d
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-04-13 14:04:22 -0300

    Responsive sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,7 +2,7 @@ import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import { ApexOptions } from "apexcharts";
 import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
-import { SideBar } from "../components/Sidebar";
+import { Sidebar } from "../components/Sidebar";
 
 const Chart = dynamic(() => import("react-apexcharts"), {
   ssr: false,
@@ -63,7 +63,7 @@ export default function Dashboard() {
       <Header />
 
       <Flex w="100%" my="6" maxWidth="1480" mx="auto" px="6">
-        <SideBar />
+        <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" aling="flex-start">
           <Box p="8" bg="gray.800" borderRadius={8} pb="4">
